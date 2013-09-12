3ae8b0f53dd6cabc564c9f4e3f4cbcba3a6cda54
nbyarley <nbyarley@fullsail.edu>
2013-09-12 18:57:36 -0400

    final submit, still need to fix a few things like localstorage being edited.

--- js/main.js
+++ js/main.js
@@ -7,57 +7,11 @@
 //Added sections for each page. these are just the basics as they will change as needed through the remaining weeks.
 
 $('#homepg').on('pageinit', function() {
-});
 
-$('#addItem').on('pageinit', function() {
-		var myForm = $('#equipmentForm');
-			myForm.validate({
-				invalidHandler: function(form, validator) {
-					},
-				submitHandler: function(form) {
-				var data = myForm.serializeArray();
-				saveData(this.key);//data
-				}
-		
-			});
-	//save data function
-	var saveData = function(data, key) {
-		if(!key) {
-		    //if there is no key it means this is a new items and needs a new key.
-			var randomID = Math.floor(Math.random()*10000001);
-		}else {
-		//set the id to the existing key were editing so that it will save over the data
-		//the key is the same key thats been passed alon fron the edit buttin even handler
-		//to the validate funtion, and then pass here into the store data function.
-			randomID = key;
-		}
-		
-		//get form information and store within an object
-		var equipment = {};
-			equipment.equipmentName = [$("#ename").val()];
-			equipment.itemList = [$("#itemList").val()];
-			equipment.rarity = [$("input:radio[name=rarity]:checked").val()];
-			equipment.levelSlide = [$("#islide").val()];
-			equipment.addNote = [$("#note").val()];
-			
-			//convert object to string
-			localStorage.setItem(randomID, JSON.stringify(equipment));
-			
-			//notify the user, equipment has been added
-			if(localStorage.length === 0){
-				alert("Equipment not saved");
-			} else {
-				alert("Equipment has been added to localStorage")
-
-			}
-		window.location.reload();
-	};
-
-});
-
-$('#equipmentNameSearch').on('pageinit', function(){
+	//using ajax to pull data from json file
 	$('#json').on('click', function(){
-		$('#equipListings').empty();
+		$('#dataListings').empty();
+		$.mobile.changePage('#dataSearch');//sen user to selected page.
 		$.ajax({
 			url		 : 'xhr/data.json',
 			type	 : 'GET',
@@ -67,19 +21,21 @@ $('#equipmentNameSearch').on('pageinit', function(){
 					var equip = jsondata.equipment[i];
 					$(''+
 						'<div class="listing">' +
-				          '<h2>' + 'Equipment Name: ' + equip.equipmentName +'</h2>' +
+				          '<h3>' + 'Equipment Name: ' + equip.equipmentName +'</h3>' +
 				          '<p>' + 'Item Slot: ' + equip.itemList +'</p>' +
-				          '<p>' + 'Rarity: ' + equip.rarity +'</p>' +
 				          '<p>' + 'Level: ' + equip.levelSlide +'</p>' +
 				          '<p>' + 'Note: ' + equip.addNote +'</p>' +
 				        '</div>'
-				    ).appendTo('#equipListings');
+				    ).appendTo('#dataListings');
 				}
 			}
 		});
 	});
+
+	//using ajax to pull data from xml file
 	$('#xml').on('click', function(){
-		$('#equipListings').empty();
+		$('#dataListings').empty();
+		$.mobile.changePage('#dataSearch');
 		$.ajax({
 			url		 : 'xhr/data.xml',
 			type 	 : 'GET',
@@ -88,106 +44,158 @@ $('#equipmentNameSearch').on('pageinit', function(){
 				$(xml).find('item').each(function() {
 					var equipmentName = $(this).find('equipmentName').text();
 					var itemList = $(this).find('itemList').text();
-					var rarity = $(this).find('rarity').text();
 					var levelSlide = $(this).find('levelSlide').text();
 					var addNote = $(this).find('addNote').text();
 					$(''+
 					'<div class="listing">' +
-						'<h2>' + 'Equipment Name: ' + equipmentName +'</h2>' +
+						'<h3>' + 'Equipment Name: ' + equipmentName +'</h3>' +
 						'<p>' + 'Item Slot: ' + itemList +'</p>' +
-						'<p>' + 'Rarity: ' + rarity +'</p>' +
 						'<p>' + 'Level: ' + levelSlide +'</p>' +
 						'<p>' + 'Note: ' + addNote +'</p>' +
-					'</div>').appendTo('#equipListings');
+					'</div>').appendTo('#dataListings');
 				});
 			}
 
 		});
 	});
-});	
 
-$('#slotSearch').on('pageinit', function(){
+		//localStorage function get, edit and delete localstorage
+		//get data (localstorage)
 	$('#lstorage').on('click', function(){
+
 		//Clears the field before it repoplulates it with new data
 		//this will prevent duplicates
-		$('#slotListings').empty();
+		$.mobile.changePage('#equipmentNameSearch');
+		//Empty the div to 
+		$('#dataListings').empty();
 		//for loop to continue through localStorage for all items.
 		for( var i=0, ls = localStorage.length; i < ls; i++) {
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
 
-		var objectString = JSON.parse(value);
+		//var objectString = JSON.parse(value);
+		var objectString = $.parseJSON(value);
 		//console.log(objectString);
 
-			var subList = $(''+
+			$(''+
 				'<div class="listing">' +
 					'<h3>' + 'Equipment Name: ' + objectString.equipmentName +'</h3>' +
 					'<p>' + 'Item Slot: ' + objectString.itemList +'</p>' +
-					'<p>' + 'Rarity: ' + objectString.rarity +'</p>' +
 					'<p>' + 'Level: ' + objectString.levelSlide +'</p>' +
 					'<p>' + 'Note: ' + objectString.addNote +'</p>' +
-					'<div class="ui-block-b">' + '<input type="button" class="delete" value="Delete" id="' + key + '"/>' + '</div>'+
-					'<div class="ui-block-b">' + '<input type="button" class="edit" value="Edit" id="' + key + '"/>' + '</div>'+
+			//Edited these out untill i am able to get localstorage to pull to form correctly.
+					//'<div class="ui-block-b">' + '<input type="button" class="delete" value="Delete" data-key="' + key + '"/>' + '</div>'+
+					//'<div class="ui-block-b">' + '<input type="button" class="edit" value="Edit" data-key="' + key + '">' + '</div>'+
 					'<br />'+
+					'<br />' +
 				'</div>'
-			).appendTo('#slotListings');
+			).appendTo('#dataListings'); // attach to the dataListing div
+
 
 			//delete function
 			$('.delete').on('click', function(){
 				var answer = ('Are you sure you want to delete this item?');
 				if (answer){
-					var dKey = this.id;
-					localStorage.removeItem(dKey);
-					window.location.reload();
+					var vkey = $(this).data('key');
+					//console.log(vkey);
+					localStorage.removeItem(vkey);
+					//window.location.href="#dataSearch";
 				}
-
-				
 			});
 
 			//edit function
-			$('.edit').on('click', function(){
+			$('.edit').on('click', function(key, value){
 				$.mobile.changePage('#addItem');
-				var eKey = $(this).attr('id');
-				
+
+				var ekey = $(this).attr('data-key');
+				//console.log(ekey);
 				$('#ename').val(objectString.equipmentName[0]);
 				$('#itemList').val(objectString.itemList[0]);
-            	var radioBtn = document.forms[0].rarity;
-				for(var i = 0; i < radioBtn.length; i++){
-					if(radioBtn[i].value == "Common" && equipment.rarity[1] == "Common") {
-						radioBtn[i].setAttribute("checked", "checked");
-					}else if(radioBtn[i].value == "Uncommon" && equipment.rarity[1] == "Uncommon") {
-						radioBtn[i].setAttribute("checked", "checked");
-					}else if(radioBtn[i].value == "Rare" && equipment.rarity[1] == "Rare") {
-						radioBtn[i].setAttribute("checked", "checked");
-					}else if (radioBtn[i].value == "Epic" && equipment.rarity[1] == "Epic"){
-						radioBtn[i].setAttribute("checked", "checked");
-					}
-				}
             	$('#islide').val(objectString.levelSlide[0]);
             	$('#note').val(objectString.addNote[0]);
-				$('#key').val(objectString);
+				$('#key').val(ekey);
 			});
 		}
-	})
+	});
 });
 
+//Add item page, and verify required fields are filled
+$('#addItem').on('pageinit', function() {
 
+		var myForm = $('#equipmentForm');
+			myForm.validate({
+				invalidHandler: function(form, validator) {
+					},
+				submitHandler: function(form) {
+				var data = myForm.serializeArray();
+				saveData(data);//data
+				}
+		
+			});
+
+			//save data to localstorage
+var saveData = function (key, value){
+	if($('#key').val() == '') {
+		var randomID = Math.floor(Math.random()*10000001);
+        console.log(randomID);
+    } else {
+    	var randomID = $('#key').val();
+    };
+    //get form information and store within an object
+		var equipment = {};
+			equipment.equipmentName = [$("#ename").val()];
+			equipment.itemList = [$("#itemList").val()];
+			equipment.levelSlide = [$("#islide").val()];
+			equipment.addNote = [$("#note").val()];
+			console.log(equipment);
+			//convert object to string
+			localStorage.setItem(randomID, JSON.stringify(equipment));
+
+			var randomID = key;
+
+			//notify the user, equipment has been added
+			if(localStorage.length === 0){
+				alert("Equipment not saved");
+			} else {
+				alert("Equipment has been added to localStorage")
+
+			}
+
+		window.location.reload();
+};
+
+   //submit click, calls saveData.
+	var submit = $('#submit');
+	submit.on('click', saveData);
+
+}); //end addItem function
+
+$('#equipmentNameSearch').on('pageinit', function(){
+});	
+
+$('#slotSearch').on('pageinit', function(){
+});
+
+$('#levelSearch').on('pageinit', function(){
+});
+
+$('#dataSearch').on('pageinit', function(){
+
+	
+});
 
 //Thsi will clear the localstorage
 $("#clearLocalStorage").on('click', function() {
+	//if statement based on length of localstorage
 	if(localStorage.length === 0){
 		alert("There is nothing to delete");
 	}else {
     	var verify = confirm("Are you sure you want to clear the localStorage?");
-	}
- 
+	};
+	//if you confirm you wish to delete, then storage will be cleared.
     if (verify) {
-        localStorage.clear();
+        localStorage.clear(); //clears localstorage
     }
-    location.refresh();
-});
-
-$('#levelSearch').on('pageinit', function(){
 });
 
 $('#about').on('pageinit', function(){
@@ -198,9 +206,5 @@ $('#about').on('pageinit', function(){
     	$('#' + $(this).attr('data-href')).show();
 	});
 });
-
-(function(){
-	$('#deleteItem').bind('click', deleteLocalItem);
-})
 
 
